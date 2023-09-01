cd2460df51c0324feb44e58d11af875573764129
abduljasim <81706041+abduljasim@users.noreply.github.com>
2023-09-01 10:02:38 -0500

    Update MHBmobileLogin.java

--- mhbmobile/src/main/java/mhbmobile/MHBmobileLogin.java
+++ mhbmobile/src/main/java/mhbmobile/MHBmobileLogin.java
@@ -4,6 +4,8 @@ import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.net.MalformedURLException;
@@ -15,18 +17,35 @@ public class MHBmobileLogin {
     static IOSDriver driver = null;
     DesiredCapabilities dc = new DesiredCapabilities();
 
-
 	@BeforeMethod
     public void setUp() throws MalformedURLException {
+
         dc.setCapability("testName","MHB login test");
         dc.setCapability("accessKey", accessKey);
         dc.setCapability("deviceQuery", "@os='ios' and @category='PHONE'");
         dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.mobileheartbeat.mhcure.enterprise");
-        driver = new IOSDriver(new URL("https://hca-new.experitest.com/wd/hub"), dc);
-        driver.findElement(By.id("LOGIN_USER_INPUT")).sendKeys("corpsvctst1");
-        driver.findElement(By.id("LOGIN_PW_INPUT")).sendKeys("Enabled1");
-        driver.findElement(By.id("Login")).click();
-   	
+        driver = new IOSDriver(new URL("https://hca-new.experitest.com/wd/hub"), dc);  
+        
+        // Unsupported Network Pop-Up
+		if(new WebDriverWait(driver, 50).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='Unsupported Network']"))).isDisplayed()){
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='DIALOGUE_SHORT_TITLE_SUBMIT_BUTTON']"))).click();
+	        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("LOGIN_USER_INPUT"))).sendKeys("hcaqa-351s");
+	        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("Login"))).click();
+	        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("DIALOGUE_SHORT_TITLE_SUBMIT_BUTTON"))).click();
+	        driver.findElement(By.id("LOGIN_USER_INPUT")).sendKeys("corpsvctst1");
+	        driver.findElement(By.id("LOGIN_PW_INPUT")).sendKeys("Enabled1");
+	        driver.findElement(By.id("Login")).click(); 
+		} else {
+//			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='DIALOGUE_SHORT_TITLE_SUBMIT_BUTTON']"))).click();
+	        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("LOGIN_USER_INPUT"))).sendKeys("hcaqa-351s");
+	        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("Login"))).click();
+	        new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("DIALOGUE_SHORT_TITLE_SUBMIT_BUTTON"))).click();
+	        driver.findElement(By.id("LOGIN_USER_INPUT")).sendKeys("corpsvctst1");
+	        driver.findElement(By.id("LOGIN_PW_INPUT")).sendKeys("Enabled1");
+	        driver.findElement(By.id("Login")).click(); 
+		}
+		
+
     }
 	
     // Test annotation is in the main class MHBmobileAutomation.java
